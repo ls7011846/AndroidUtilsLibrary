d6c2e841f7d2c644ef013b0b4d8abd0d4c5e6ead
GuoChao <smartbetter@users.noreply.github.com>
2017-01-16 11:23:30 +0800

    Update SPUtils.java

--- UtilsLibrary/src/main/java/com/gc/utils/system/SPUtils.java
+++ UtilsLibrary/src/main/java/com/gc/utils/system/SPUtils.java
@@ -16,11 +16,10 @@ package com.gc.utils.system;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
+ * API 9 及以上
  * Created by gc on 2016/11/6.
  */
 public class SPUtils {
@@ -37,77 +36,71 @@ public class SPUtils {
     /**
      * 保存数据的方法，根据类型调用不同的保存方法
      */
-    public static void put(Context context, String key, Object object) {
-
-        SharedPreferences sp = context.getSharedPreferences(FILE_NAME,
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-
-        if (object instanceof String) {
-            editor.putString(key, (String) object);
-        } else if (object instanceof Integer) {
-            editor.putInt(key, (Integer) object);
-        } else if (object instanceof Boolean) {
-            editor.putBoolean(key, (Boolean) object);
-        } else if (object instanceof Float) {
-            editor.putFloat(key, (Float) object);
-        } else if (object instanceof Long) {
-            editor.putLong(key, (Long) object);
-        } else {
-            editor.putString(key, object.toString());
-        }
-        SharedPreferencesCompat.apply(editor); //commit同步的，apply异步的
+    public static void putString(Context context, String key, String value) {
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        sp.edit().putString(key, value).apply();//commit同步的，apply异步的
+    }
+    public static void putInt(Context context, String key, int value) {
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        sp.edit().putInt(key, value).apply();
+    }
+    public static void putBoolean(Context context, String key, boolean value) {
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        sp.edit().putBoolean(key, value).apply();
+    }
+    public static void putFloat(Context context, String key, float value) {
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        sp.edit().putFloat(key, value).apply();
+    }
+    public static void putLong(Context context, String key, long value) {
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        sp.edit().putLong(key, value).apply();
     }
 
     /**
      * 获取数据的方法，根据默认值得到数据的类型，然后调用对应方法获取值
      */
-    public static Object get(Context context, String key, Object defaultObject) {
-        SharedPreferences sp = context.getSharedPreferences(FILE_NAME,
-                Context.MODE_PRIVATE);
-
-        if (defaultObject instanceof String) {
-            return sp.getString(key, (String) defaultObject);
-        } else if (defaultObject instanceof Integer) {
-            return sp.getInt(key, (Integer) defaultObject);
-        } else if (defaultObject instanceof Boolean) {
-            return sp.getBoolean(key, (Boolean) defaultObject);
-        } else if (defaultObject instanceof Float) {
-            return sp.getFloat(key, (Float) defaultObject);
-        } else if (defaultObject instanceof Long) {
-            return sp.getLong(key, (Long) defaultObject);
-        }
-        return null;
+    public static String getString(Context context,String key,String defValue){
+        SharedPreferences sp=context.getSharedPreferences(FILE_NAME,Context.MODE_PRIVATE);
+        return sp.getString(key,defValue);
+    }
+    public static int getInt(Context context,String key,int defValue){
+        SharedPreferences sp=context.getSharedPreferences(FILE_NAME,Context.MODE_PRIVATE);
+        return sp.getInt(key,defValue);
+    }
+    public static boolean getBoolean(Context context,String key,boolean defValue){
+        SharedPreferences sp=context.getSharedPreferences(FILE_NAME,Context.MODE_PRIVATE);
+        return sp.getBoolean(key,defValue);
+    }
+    public static float getFloat(Context context,String key,float defValue){
+        SharedPreferences sp=context.getSharedPreferences(FILE_NAME,Context.MODE_PRIVATE);
+        return sp.getFloat(key,defValue);
+    }public static long getLong(Context context,String key,long defValue){
+        SharedPreferences sp=context.getSharedPreferences(FILE_NAME,Context.MODE_PRIVATE);
+        return sp.getLong(key,defValue);
     }
 
     /**
      * 移除某个key对应的值
      */
     public static void remove(Context context, String key) {
-        SharedPreferences sp = context.getSharedPreferences(FILE_NAME,
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.remove(key);
-        SharedPreferencesCompat.apply(editor);
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        sp.edit().remove(key).apply();
     }
 
     /**
      * 清除所有数据
      */
     public static void clear(Context context) {
-        SharedPreferences sp = context.getSharedPreferences(FILE_NAME,
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.clear();
-        SharedPreferencesCompat.apply(editor);
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        sp.edit().clear().apply();
     }
 
     /**
      * 查询某个key是否已经存在
      */
     public static boolean contains(Context context, String key) {
-        SharedPreferences sp = context.getSharedPreferences(FILE_NAME,
-                Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
         return sp.contains(key);
     }
 
@@ -115,45 +108,8 @@ public class SPUtils {
      * 返回所有的键值对
      */
     public static Map<String, ?> getAll(Context context) {
-        SharedPreferences sp = context.getSharedPreferences(FILE_NAME,
-                Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
         return sp.getAll();
-    }
-
-    /**
-     * 创建一个解决SharedPreferencesCompat.apply方法的一个兼容类（适配）
-     */
-    private static class SharedPreferencesCompat {
-        private static final Method sApplyMethod = findApplyMethod();
-
-        /**
-         * 反射查找apply的方法
-         */
-        @SuppressWarnings({"unchecked", "rawtypes"})
-        private static Method findApplyMethod() {
-            try {
-                Class clz = SharedPreferences.Editor.class;
-                return clz.getMethod("apply");
-            } catch (NoSuchMethodException e) {
-            }
-            return null;
-        }
-
-        /**
-         * 如果找到则使用apply执行，否则使用commit
-         */
-        public static void apply(SharedPreferences.Editor editor) {
-            try {
-                if (sApplyMethod != null) {
-                    sApplyMethod.invoke(editor);
-                    return;
-                }
-            } catch (IllegalArgumentException e) {
-            } catch (IllegalAccessException e) {
-            } catch (InvocationTargetException e) {
-            }
-            editor.commit();
-        }
     }
 
 }
